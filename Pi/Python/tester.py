742c861a7a19dd2180ce2b000bf18876502b2864
John Walthour <john.walthour@gmail.com>
2017-03-08 23:03:52 -0500

    a tester for the Max31855 obect

---
+++ Pi/Python/tester.py
@@ -0,0 +1,8 @@
+import max31855
+import time
+m = max31855.Max31855()
+
+while True:
+	m.take_reading()
+	print("Fault? %s Therm: %f Chip: %f Fault: %s"%(str(m.is_faulted()), m.thermocouple_temp_f(), m.internal_temp_f(), m.fault_reason()))
+	time.sleep(0.1)
